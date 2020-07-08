6a3374ffc4bfb8fbc4985893c00f360cc2350b62
JenniferGoijman <jgoijman@gmail.com>
2020-07-08 15:47:41 +0200

    first commit

---
+++ frontend/src/containers/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tiles from '../Components/Tiles';
+
+
+export const Game = () => {
+    return (
+        <div>
+            <Tiles></Tiles>
+        </div>
+    )
+}
+
+export default Game;
